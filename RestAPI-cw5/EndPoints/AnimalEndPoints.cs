5ff5775636008da1d9ec5b58cea2d86bb405a12f
s27977 <s27977@pjwstk.edu.pl>
2024-04-09 16:38:12 +0200

    zajecia

---
+++ RestAPI-cw5/EndPoints/AnimalEndPoints.cs
@@ -0,0 +1,14 @@
+﻿namespace RestAPI_cw5.EndPoints;
+
+public static class AnimalEndPoints
+{
+    public static void MapAnimalEndPoints(this WebApplication app)
+    {
+        app.MapGet("/animals/{id}", (int id) =>
+        {
+            // 200 - OK
+            // 404 - Not Found
+            return Results.Ok(id);
+        });
+    }
+}
---
+++ RestAPI-cw5/Program.cs
@@ -0,0 +1,28 @@
+using RestAPI_cw5.EndPoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//miniaml API
+app.MapAnimalEndPoints();
+
+// Controllers
+
+
+app.Run();
+
